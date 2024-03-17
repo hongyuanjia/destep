fd565dc02c675202b0971a3673433ab85815423f
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2024-03-17 14:49:19 +0800

    test(read): add tests for `read_dest()`

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -1,3 +1,20 @@
 test_that("read_dest()", {
-    expect_equal(1L, 1L)
+    skip_on_cran()
+    path <- ensure_dest_test_file()
+
+    # can stop if invalid file input
+    expect_error(dest <- read_dest(1), "single file path")
+
+    # can stop if file does not exist
+    expect_error(dest <- read_dest(tempfile()), "did not exist")
+
+    # can read specific tables
+    expect_s4_class(dest <- read_dest(path, "WINDOW"), "SQLiteConnection")
+    expect_equal(DBI::dbListTables(dest), "WINDOW")
+    DBI::dbDisconnect(dest)
+
+    # can read all tables
+    expect_s4_class(dest <- read_dest(path, verbose = FALSE), "SQLiteConnection")
+    expect_true("WINDOW" %in% DBI::dbListTables(dest))
+    DBI::dbDisconnect(dest)
 })
